4d9488798b9068525c2575d27cbba81db4826d65
7Jay-Lee7 <jay_lee777@outlook.com>
2023-08-06 19:15:45 +0100

    first commit

---
+++ components/shared/Topbar.tsx
@@ -0,0 +1,43 @@
+import { OrganizationSwitcher, SignUpButton, SignedIn } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+import Image from "next/image";
+import Link from "next/link";
+
+function Topbar() {
+  return (
+    <nav className="topbar">
+      <Link href="/" className="flex items-center gap-4">
+        <Image src="/logo.png" alt="logo" width={28} height={28} />
+        <p className="text-heading3-bold text-light-1 max-xs:hidden">Threads</p>
+      </Link>
+
+      <div className="flex items-center gap-1">
+        <div className="block md:hidden">
+          <SignedIn>
+            <SignUpButton>
+              <div className="flex cursor-pointer">
+                <Image
+                  src="/assets/logout.svg"
+                  alt="alt"
+                  width={24}
+                  height={24}
+                />
+              </div>
+            </SignUpButton>
+          </SignedIn>
+        </div>
+
+        <OrganizationSwitcher
+          appearance={{
+            baseTheme: dark,
+            elements: {
+              organizerSwitcherTrigger: "py-2 px-4",
+            },
+          }}
+        />
+      </div>
+    </nav>
+  );
+}
+
+export default Topbar;
